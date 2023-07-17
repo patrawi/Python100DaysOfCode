0eac921ec3b4b0a966ff76f192787213924605e5
Pirawat Wareetanyarat <peaphee123456789@gmail.com>
2023-07-17 13:20:10 +0700

    feat: day2

---
+++ d2/main.py
@@ -0,0 +1,7 @@
+print('Welcome to the tip calculator.')
+total_bill = float(input('What was the total bill? $ '))
+percentage_tip = int(
+    input('What percentage tip would you like to give? 10, 12, or 15? '))
+number_people = int(input('How many people to split the bill? '))
+person = total_bill * (1 + percentage_tip / 100) / number_people
+print(f'Each person should pay: ${round(person, 2)}')
